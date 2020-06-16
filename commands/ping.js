e71d4d27a6763dab6837dc49ba154fb02a773dda
Glitch (no-fortnite) <none>
2020-06-16 07:43:58 +0000

    🌱🕰 Checkpoint ./commands/ping.js:6429734/292 ./watch.json:6429734/2

--- commands/ping.js
+++ commands/ping.js
@@ -11,18 +11,12 @@ module.exports = {
           const now = performance.now()
          await ping.get("test")
           const read = performance.now()
-          const beforeWrite = performance.now()
-          await ping.set("test",{
-            test:performance.now()
-          })
-          const written = performance.now()
             m.edit(
                 `
                 ========PONG!=========
-• Message round trip                        :: ${Math.round(Date.now() - m.createdTimestamp - read + now - written + beforeWrite)} ms 
+• Message round trip                        :: ${Math.round(Date.now() - m.createdTimestamp - read + now)} ms 
 • Discord API heartbeat                     :: ${Math.round(message.client.ws.ping)} ms
-• Database (read)                           :: ${(read - now).toFixed(2)} ms
-• Database (write)                          :: ${(written - beforeWrite).toFixed(2)} ms`,         
+• Database (read)                           :: ${(read - now).toFixed(2)} ms`,       
                 { code: "asciidoc" }
             );
         });
