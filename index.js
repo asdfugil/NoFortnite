27d3e735a06deaac7db4f9784bbd202c516e64f7
Glitch (no-fortnite) <none>
2020-06-19 08:16:49 +0000

    🍙🚆 Checkpoint ./index.js:6429734/177 ./watch.json:6429734/2

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const { ShardingManager } = require('discord.js')
 const manager = new ShardingManager("./bot.js", { token: process.env.BOT_TOKEN })
 manager.spawn()
 manager.on('launch',shard => console.log("Launched shard " + shard.id))
+process.on("unhandledRejection",(error,promise) => {
+  console.error(error)
+  if (error.message === "<#Response>") process.exit(1)
+})
